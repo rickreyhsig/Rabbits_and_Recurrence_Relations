de60171e24f0af2ee3f1aac223cf42a5285f1940
Ricardo Kreyhsig <rickreyhsig@gmail.com>
2015-02-12 13:57:10 -0500

    Finalized script

--- Rabbits_and_Recurrence_Relations.rb
+++ Rabbits_and_Recurrence_Relations.rb
@@ -4,6 +4,7 @@
 n = ARGV[0]
 k = ARGV[1]
 
+# Function to solve problem.. a slight modification of the Fibonacci sequence
 def fib_k(n, k)
 	return n if ( n == 0 || n ==1 )
   ( fib_k( n - 1 , k ) + fib_k( n - 2 , k )*k )
