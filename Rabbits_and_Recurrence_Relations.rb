152b684d8c39bcb1263fb759c543466f610de87b
Ricardo Kreyhsig <rickreyhsig@gmail.com>
2015-02-12 13:54:11 -0500

    Ruby script that solves issue[C

---
+++ Rabbits_and_Recurrence_Relations.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+
+# Obtain n and k
+n = ARGV[0]
+k = ARGV[1]
+
+def fib_k(n, k)
+	return n if ( n == 0 || n ==1 )
+  ( fib_k( n - 1 , k ) + fib_k( n - 2 , k )*k )
+end
+
+puts fib_k(n.to_i, k.to_i)
